31dd0f40fa58888a20ae5df493a20f765b77d7e9
Atticus Kuhn <atticusmkuhn@atticsmkuhn.com>
2025-07-10 19:02:37 -0700

    add variable V

--- Autoomaton/attempt2.lean
+++ Autoomaton/attempt2.lean
@@ -36,43 +36,44 @@ structure RankingFunction {S : Type} (A : Automaton S) : Type where
   C2 : ∀ (s : S), ¬ A.F s → ∀ (s' : S), A.R s s' → V s' ≤ V s
   C4 : ∀ (s1 s2 : S), A.R s1 s2 → V s2 + (if A.F s1 then 1 else 0) ≤ V s1
 
+variable (V : RankingFunction a)
+
 @[simp, reducible]
 def fairVisits : Set ℕ := { n : Nat | a.F (r.f n) }
 
 noncomputable def nth_visit (n : Nat) : Nat := Nat.nth (fun m => a.F (r.f m)) n
 
-lemma BoundedFairVisits36 (V : RankingFunction a) (r : Run a)  :  nth_visit a r (V.V (r.f 0)) ∈ upperBounds (fairVisits a r) := by
+lemma BoundedFairVisits36 : nth_visit a r (V.V (r.f 0)) ∈ upperBounds (fairVisits a r) := by
   simp [BddAbove, upperBounds, Nonempty]
   intros x x_fair
   simp [nth_visit]
   sorry
 
-lemma BoundedFairVisits38 (V : RankingFunction a) (r : Run a) (y : Nat) : V.V (r.f y) + Nat.count (fun m => a.F (r.f m)) y ≤ V.V (r.f 0)  := by
+lemma BoundedFairVisits38 (y : Nat) : V.V (r.f y) + Nat.count (fun m => a.F (r.f m)) y ≤ V.V (r.f 0)  := by
   induction' y with i ih
   · simp only [Nat.count_zero, add_zero, le_refl]
   · simp only [Nat.count_succ]
     have n : V.V (r.f (i + 1)) + (if a.F (r.f i) then 1 else 0) ≤ V.V (r.f i) := V.C4 (r.f i) (r.f (i + 1)) (r.IsValid i)
     omega
 
-lemma BoundedFairVisits39 (r : Run a) (inf : Set.Infinite (fairVisits a r)) (y : Nat) :  Nat.count (fun m => a.F (r.f m)) (nth_visit a r y) = y := by
+lemma BoundedFairVisits39 (inf : Set.Infinite (fairVisits a r)) (y : Nat) :  Nat.count (fun m => a.F (r.f m)) (nth_visit a r y) = y := by
   exact Nat.count_nth (by
     intro hf
     contradiction)
 
-lemma BoundedFairVisits310 (V : RankingFunction a) (r : Run a) (inf : Set.Infinite (fairVisits a r)) : V.V (r.f (nth_visit a r (V.V (r.f 0)))) = 0  := by
-  have x1 := BoundedFairVisits38 a V r (nth_visit a r (V.V (r.f 0)))
+lemma BoundedFairVisits310 (inf : Set.Infinite (fairVisits a r)) : V.V (r.f (nth_visit a r (V.V (r.f 0)))) = 0  := by
+  have x1 := BoundedFairVisits38 a r V (nth_visit a r (V.V (r.f 0)))
   simp only [BoundedFairVisits39 a r inf (V.V (r.f 0)), add_le_iff_nonpos_left, nonpos_iff_eq_zero] at x1
   exact x1
 
-lemma BoundedFairVisits3 (V : RankingFunction a) (r : Run a)  :  Set.Finite (fairVisits a r) := by
+lemma BoundedFairVisits3 (V : RankingFunction a) :  Set.Finite (fairVisits a r) := by
   have y :  BddAbove (fairVisits a r) := by
-    -- simp [BddAbove, upperBounds, Nonempty, ]
     apply (Set.nonempty_of_mem)
-    exact BoundedFairVisits36 a V r
+    exact BoundedFairVisits36 a r V
   exact BddAbove.finite y
 
 @[simp, reducible]
-noncomputable def fairVisits2 (V : RankingFunction a) : Finset ℕ :=  Set.Finite.toFinset (BoundedFairVisits3 a V r)
+noncomputable def fairVisits2 (V : RankingFunction a) : Finset ℕ :=  Set.Finite.toFinset (BoundedFairVisits3 a r V)
 
 theorem Soundness (V : RankingFunction a) : a.IsFairEmpty :=  by
   intros r
